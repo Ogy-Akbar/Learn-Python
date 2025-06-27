678e1a670ea9be6cf084a4d81070fd7ff783ffe5
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-06-27 13:56:07 +0700

    Create 6-Variable Names.py

---
+++ W3-Schools/Basic-Python/6-Variable Names.py
@@ -0,0 +1,25 @@
+#6 Python - Variable Names
+
+'''
+* Variable can have a short name (like x or y) or more descriptive name.
+* There are rules for python variables:
+
+- A variable name must start with a letter or the underscore 		  character
+- A variable name cannot start with a number
+- A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+- Variable names are case-sensitive (age, Age and AGE are three different variables)
+- A variable name cannot be any of the Python keywords.
+
+* There are three techniques to name a variable with more than one word (multi words:
+
+ - Camel Case: Each word capital without space, except the first word not capital
+	ex: myVariableName = "John"
+
+- Pascal Case: Each word starts with capital
+	ex: MyVariableName = "John"
+
+- Snake Case: each word is separated by an underscore:
+	ex: my_variable_name = "John"
+ '''
+
+
