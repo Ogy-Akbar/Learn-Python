ff3c6b78da29f9e87bacde250691abcc73002f59
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-06-27 13:49:31 +0700

    Create 4-Comments.py

---
+++ W3-Schools/Basic-Python/4-Comments.py
@@ -0,0 +1,15 @@
+#4 Python Comments
+'''
+- Use a # to write a single line comment:
+	#print("Hello, World!")
+	print("Cheers, Mate!")
+
+- Use a """ and ended with """ to write a multiline of comments
+	"""
+	This is a comment
+	written in
+	more than just one line
+	"""
+	print("Hello, World!")
+ '''
+
