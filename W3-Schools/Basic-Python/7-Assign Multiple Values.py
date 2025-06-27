313acf7693d3e58c32f6e1caab6cfcca92dc9589
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-06-27 13:57:18 +0700

    Create 7-Assign Multiple Values.py

---
+++ W3-Schools/Basic-Python/7-Assign Multiple Values.py
@@ -0,0 +1,26 @@
+#7 Python Variables - Assign Multiple Values
+
+'''
+* Many Vaues to Multiple variables in one line
+	ex: 
+	x, y, z = "Orange", "Banana", "Cherry"
+	print(x)
+	print(y)
+	print(z)
+	
+* One Value to Multiple Variables
+	ex: 
+	x = y = z = "Orange"
+	print(x)
+	print(y)
+	print(z)
+
+* Unpacking a Collection: Collection of values can be extracted into variables
+
+	ex: 
+	fruits = ["apple", "banana", "cherry"]
+	x, y, z = fruits
+	print(x)
+	print(y)
+	print(z)
+'''
