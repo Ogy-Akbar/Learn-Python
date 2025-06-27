ffebdbd0d51cdfbb35c0426f11524bc33542311e
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-06-27 13:52:11 +0700

    Create 5-Variables.py

---
+++ W3-Schools/Basic-Python/5-Variables.py
@@ -0,0 +1,31 @@
+#5 Python Variables
+
+'''
+* Python has no command for declaring a variable, Variable do not need to be declared with any particular type and can even change type after they have been set
+
+x = 5
+y = "John"
+print(x)
+print(y)
+
+* Casting: To specify the data type of a variable
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+* Get The Type: can get the data type of variable with the type() function
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+
+* Quotes: String canbe declared using single or double quotes
+x = "John"
+# is the same as
+x = 'John'
+
+* Case Sensitive: Two variable of a and A are different variables
+a = 4
+A = "Sally"
+#A will not overwrite a
+'''
