6618e9a43da1bf3da649f2c23840017ba57c7207
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-06-27 13:46:07 +0700

    Create 3-Syntax.py

---
+++ W3-Schools/Basic-Python/3-Syntax.py
@@ -0,0 +1,16 @@
+#3 Python Syntax
+
+'''
+* Python Indentation:
+- Python uses indentation to indicate a block of code
+- If Indentation is skipped the python will give an error
+- Indentation has minimum of one space
+- If the number of space in a block is different then the python will give   an error
+
+* Python Variables:
+- Variables are created when you assign a value to it
+- Python has no command for declaring a variable
+
+* Python  Comments:
+- Python comments uses a # and Python will render the rest of the line as a comment
+'''
