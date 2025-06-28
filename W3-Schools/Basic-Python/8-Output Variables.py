2bc640f844a3da5c5f91f949dc2da368110eb411
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-06-28 13:18:32 +0700

    Create 8-Output Variables.py

---
+++ W3-Schools/Basic-Python/8-Output Variables.py
@@ -0,0 +1,27 @@
+#8 Python - Output Variables
+
+'''
+* print() function is used to output variables
+	ex: 
+	x = "Python is awesome"
+	print(x)
+
+* print() to output multiple variables separated by commas
+	ex:
+	x = "Python"
+	y = "is"
+	z = "awesome"
+	print(x, y, z)
+
+* use the + operator to output multiple variables
+	ex: 
+	x = "Python "
+	y = "is "
+	z = "awesome"
+	print(x + y + z)
+ '''
+	
+	remember to use space after the word (except the last word) so the 	final result won't combined. except comma, it will give space 	automatically
+
+* Python will give error if try to combine two different types of variables ex: number + string. The only to combine it is by giving comma instead of +
+
