f7a36b8713cdc803ae3f9b08a889b245f447ad57
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-04 21:11:39 +0700

    Create 1.4-Modify Strings.py

---
+++ W3-Schools/Basic-Python/1.4-Modify Strings.py
@@ -0,0 +1,33 @@
+#15 Python - Modify Strings
+
+'''
+* Upper Case, print the string in upper case all
+
+ex:
+a = "Hello, World!"
+print(a.upper())
+
+* Lower Case, print the string in lower case all
+
+ex:
+a = "Hello, World!"
+print(a.lower())
+
+* Remove Whitespace, to remove any whitespace from the beginning or the end
+
+ex: 
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
+
+* Replace String, To replace a character or phrase in a string with a new one
+
+ex: 
+a = "Hello, World!"
+print(a.replace("H", "J"))
+
+* Split String, Split the string into sub-strings
+
+ex: 
+a = "Hello, World!"
+print(a.split(",")) # returns ['Hello', ' World!']
+'''
