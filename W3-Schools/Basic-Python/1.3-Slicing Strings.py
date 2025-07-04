ec9687a0874f34d1962da119bd5163592dd65c08
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-04 21:09:37 +0700

    Create 1.3-Slicing Strings.py

---
+++ W3-Schools/Basic-Python/1.3-Slicing Strings.py
@@ -0,0 +1,28 @@
+#14 Python - Slicing Strings
+
+'''
+* Slicing, specify the range of character for the output based on its index, separated by colon. the character will end with index before the second range.
+
+ex:
+b = "Hello, World!"
+print(b[2:5])
+
+* Slicing from the start, range starts from first character to certain index
+
+ex:
+b = "Hello, World!"
+print(b[:5])
+
+* Slicing to the End, range starts from a certain index to the last character or index
+
+ex: 
+b = "Hello, World!"
+print(b[2:])
+
+* Negative Indexing, to start the range from the end of the string
+
+ex:
+b = "Hello, World!"
+print(b[-5:-2])
+'''
+
