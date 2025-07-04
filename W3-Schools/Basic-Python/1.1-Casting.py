1f777ddc64d6906c981852ee700a04fb4cfbc44e
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-04 21:03:10 +0700

    Create 1.1-Casting.py

---
+++ W3-Schools/Basic-Python/1.1-Casting.py
@@ -0,0 +1,9 @@
+#12 Python Casting
+
+'''
+* Python Casting is to specify a variable type of a value. Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+
+int() => to construct an integer number from various data type 
+float() => to construct an float number from various data type
+str() => to construct a string from various data type
+'''
