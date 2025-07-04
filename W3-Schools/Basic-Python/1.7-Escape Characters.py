ddad406c72c3c2320fa5d9cd71c05ba9f0aab0c2
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-04 21:21:30 +0700

    Create 1.7-Escape Characters.py

---
+++ W3-Schools/Basic-Python/1.7-Escape Characters.py
@@ -0,0 +1,28 @@
+#18 Python - Escape Characters
+
+'''
+* to insert characters that are illegal in a string, use an escape character
+* The Escape Character is a backlash \ followed by the character you want to insert
+
+exp.:
+txt = "We are the so-called "Vikings" from the north."
+
+* this will return an error, so backlash is needed.
+
+exp.:
+txt = "We are the so-called \"Vikings\" from the north."
+
+Other Escape Characters are:
+\'	Single Quote	
+\\	Backslash	
+\n	New Line	
+\r	Carriage Return	
+\t	Tab	
+\b	Backspace	
+\f	Form Feed	
+\ooo	Octal value	
+\xhh	Hex value
+
+* Carriage Return is used to move the cursor to the beginning of the current line without advancing to the next line
+'''
+
