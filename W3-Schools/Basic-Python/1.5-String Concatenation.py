56f800fdcd24f8e51be25aac4e703c49e22f49db
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-04 21:14:02 +0700

    Create 1.5-String Concatenation.py

---
+++ W3-Schools/Basic-Python/1.5-String Concatenation.py
@@ -0,0 +1,19 @@
+#16 Python - String Concatenation
+
+'''
+* String Concatenation is to combine two string using the '+' operator
+
+ex: 
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+* To add a space between the concatenated strings, add a " "
+
+ex:
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+'''
