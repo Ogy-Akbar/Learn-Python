874a85865812dbc465f9dd7cd3ef29b9e3a1c8a8
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-10 13:11:39 +0700

    Create 2.4-Add List Items.py

---
+++ W3-Schools/Basic-Python/2.4-Add List Items.py
@@ -0,0 +1,33 @@
+#25 Python - Add List Items
+
+'''
+* append() method to add an item at the end of the list
+
+exp:
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+* insert() method to add an item at a specific index
+
+exp:
+thislist = ["apple", "banana", "cherry"]
+thislist.insert(1, "orange")
+print(thislist)
+
+* extend list to add list into a list, where the additional list will follow the added list
+
+exp: 
+thislist = ["apple", "banana", "cherry"]
+tropical = ["mango", "pineapple", "papaya"]
+thislist.extend(tropical)
+print(thislist)
+
+* extend() to append lists with any other iterable object (tuples, Sets, Dictionaries, etc.)
+
+exp:
+thislist = ["apple", "banana", "cherry"]
+thistuple = ("kiwi", "orange")
+thislist.extend(thistuple)
+print(thislist)
+'''
