226d4563791597ed2d9a7e69e96336ef073aeb70
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-16 21:41:34 +0700

    Create 3.5-Join Tuples.py

---
+++ W3-Schools/Basic-Python/3.5-Join Tuples.py
@@ -0,0 +1,20 @@
+#36 Python - Join Tuples
+
+# Join two or more tuples using the '+' operator
+
+'''
+tuple1 = ("a", "b" , "c")
+tuple2 = (1, 2, 3)
+
+tuple3 = tuple1 + tuple2
+print(tuple3)
+'''
+
+# Multiply the tuple items a given number of times by using '*' operator
+
+'''
+fruits = ("apple", "banana", "cherry")
+mytuple = fruits * 2
+
+print(mytuple)
+'''
