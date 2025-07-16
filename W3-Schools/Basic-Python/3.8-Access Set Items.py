264fcf7763370f180002baf8865dce1f76ce4338
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-07-16 21:47:35 +0700

    Create 3.8-Access Set Items.py

---
+++ W3-Schools/Basic-Python/3.8-Access Set Items.py
@@ -0,0 +1,28 @@
+#39 Python - Access Set Items
+
+# Cannot Access items in a set by referring to an index or a key
+# Can loop through the set items using 'for' loop
+
+'''
+thisset = {"apple", "banana", "cherry"}
+
+for x in thisset:
+  print(x)
+  
+'''
+
+# Can ask if specified value is in or not in a set by using the 'in' keyword
+
+'''
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" in thisset)
+'''
+
+'''
+thisset = {"apple", "banana", "cherry"}
+
+print("banana" not in thisset)
+'''
+
+# Change Items, Once a set is created, you cannot change its items, but you can add new items.
