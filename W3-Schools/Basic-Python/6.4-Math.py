69fe6cbb8e80f13f2c278236522098d028387491
Ogy-Akbar <ogyakbar.professional@gmail.com>
2025-09-16 10:43:07 +0700

    Create 6.4-Math.py

---
+++ W3-Schools/Basic-Python/6.4-Math.py
@@ -0,0 +1,56 @@
+#65 Python - Math
+
+# Python has a set of built-in math functions, includin extensive math module
+
+# min() and max() functions can be used to find lowest or highest value
+
+'''
+x = min(5, 10, 25)
+y = max(5, 10, 25)
+
+print(x)
+print(y)
+'''
+
+#abs() functoion to return absolute value
+
+'''
+x = abs(-7.25)
+
+print(x)
+'''
+
+'''
+x = pow(4, 3)
+
+print(x)
+'''
+
+# python has built-in module called math, whcih extend the list of mathematical functions
+
+'''
+import math
+
+x = math.sqrt(64)
+
+print(x)
+'''
+
+'''
+import math
+
+x = math.ceil(1.4)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+'''
+
+'''
+import math
+
+x = math.pi
+
+print(x)
+
+'''
